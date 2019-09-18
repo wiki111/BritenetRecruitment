31bee95ce2a3a344efb130b5bf0076d316035e86
Daniel <maciej.wiqira@gmail.com>
2019-09-18 16:22:37 +0200

    Deleted commented out testing code.

--- src/Application.java
+++ src/Application.java
@@ -14,20 +14,6 @@ import java.util.ArrayList;
 class Application{
 
     public static void main(String args[]){
-
-        /* test code System.out.println(new MySQLConnector().getCustomersFromDB());
-        CustomerContactsSaxParser parser = new CustomerContactsSaxParser();
-        parser.testParsing();
-        CustomerContactsCSVParser customerContactsCSVParser = new CustomerContactsCSVParser();
-        ArrayList<Customer> customers = (ArrayList) customerContactsCSVParser.getCustomersFromFile(new File("D:\\ProgrammingProjects\\BritenetRecruitment\\res\\exampledata\\datacsv.txt"));
-        for(Customer customer : customers){
-            System.out.println(customer.toString());
-        }
-        */
-        //Reader reader = new XMLReader();
-        //reader.readAndSaveToDB(new File("D:\\ProgrammingProjects\\BritenetRecruitment\\res\\exampledata\\dataxml.xml"), new MySQLConnector());
-        //Reader reader = new CSVReader();
-       // reader.readAndSaveToDB(new File("D:\\ProgrammingProjects\\BritenetRecruitment\\res\\exampledata\\datacsv.txt"), new MySQLConnector());
         ApplicationGUI gui = new ApplicationGUI();
         gui.run();
     }
